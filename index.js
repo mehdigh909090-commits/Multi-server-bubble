5ca54250cd9b93fb2aa869c2ff6cb96e20f379f8
Caine <mehdigh909090@gmail.com>
2026-09-05 17:07:29 +0330

    Remove duplicate server connection logic

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bedrock = require("bedrock-protocol");
 const app = express();
 
 const servers = [
-  { ip: "RLMC.ir", port: 19132, password: "ItzBubble" },
   { ip: "sv4.tgmc.ir", port: 29049, password: "ItzBubble" },
   { ip: "185.26.33.12", port: 19132, password: "ItzBubble" },
   { ip: "dreamland.falixsrv.me", port: 23110, password: "ItzBubble" }
